9c8a615733303ce83ff3c7ec64707867cad729e7
VArabchuk <arabon3@gmail.com>
2024-10-04 19:52:55 +0300

    Search books init

--- src/main/java/mate/academy/intro/repository/BookRepository.java
+++
@@ -1,7 +0,0 @@
-package mate.academy.intro.repository;
-
-import mate.academy.intro.model.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookRepository extends JpaRepository<Book, Long> {
-}
---
+++ src/main/java/mate/academy/intro/repository/book/BookSpecificationBuilder.java
@@ -0,0 +1,39 @@
+package mate.academy.intro.repository.book;
+
+import lombok.RequiredArgsConstructor;
+import mate.academy.intro.dto.BookSearchParametersDto;
+import mate.academy.intro.model.Book;
+import mate.academy.intro.repository.SpecificationBuilder;
+import mate.academy.intro.repository.SpecificationProviderManager;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class BookSpecificationBuilder implements SpecificationBuilder<Book> {
+    private final SpecificationProviderManager<Book> bookSpecificationProviderManager;
+
+    @Override
+    public Specification<Book> build(BookSearchParametersDto searchParametersDto) {
+        Specification<Book> specification = Specification.where(null);
+        if (searchParametersDto.isbn() != null && searchParametersDto.isbn().length > 0) {
+            specification
+                    = specification.and(bookSpecificationProviderManager
+                    .getSpecificationProvider("isbn")
+                    .getSpecification(searchParametersDto.isbn()));
+        }
+        if (searchParametersDto.author() != null && searchParametersDto.author().length > 0) {
+            specification
+                    = specification.and(bookSpecificationProviderManager
+                    .getSpecificationProvider("author")
+                    .getSpecification(searchParametersDto.author()));
+        }
+        if (searchParametersDto.title() != null && searchParametersDto.title().length > 0) {
+            specification
+                    = specification.and(bookSpecificationProviderManager
+                    .getSpecificationProvider("title")
+                    .getSpecification(searchParametersDto.title()));
+        }
+        return specification;
+    }
+}
